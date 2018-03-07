59b34a496a2c9ffbdcc51145bb701c15f84fbd98
xgirma <xgirma@gmail.com>
2018-03-07 02:38:41 -0800

    add /api/v1/ping

--- api/api.js
+++ api/api.js
@@ -1,5 +1,8 @@
 var router = require('express').Router();
 
+router.use('/ping', router.get('/', function(req, res, next){
+  res.status(200).send({ data: { message: "pong"}});
+}));
 router.use('/channels', require('./channels/routes'));
 router.use('/pods', require('./pods/routes'));
 
