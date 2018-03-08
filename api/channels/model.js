f13e573243e3c738169f3a70111e39226c59317c
xgirma <xgirma@gmail.com>
2018-03-08 02:17:43 -0800

    formatting

--- api/channels/model.js
+++ api/channels/model.js
@@ -2,32 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ChannelsSchema = new Schema({
-	title: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	link: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	description: {
-		type: String,
-		required: false
-	},
-	image: {
-		type: String,
-		required: false
-	},
-	author: {
-		type: String,
-		required: false
-	},
-	copyright: {
-		type: String,
-		required: false
-	}
+  title: {type: String, required: true, unique: true},
+  link: {type: String, required: true, unique: true},
+  description: {type: String, required: false},
+  image: {type: String, required: false},
+  author: {type: String, required: false},
+  copyright: {type: String, required: false}
 });
 
 module.exports = mongoose.model('channel', ChannelsSchema);
--- api/channels/routes.js
+++ api/channels/routes.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var controller = require('./controller');
 
 router.route('/')
-	.get(controller.get)
-	.post(controller.post);
+  .get(controller.get)
+  .post(controller.post);
 
 module.exports = router;
