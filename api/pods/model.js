8b3ed2f0a3124048e4fb4de717c12eca715d91d4
xgirma <xgirma@gmail.com>
2018-03-09 00:57:17 -0800

    update pods model

--- api/pods/model.js
+++ api/pods/model.js
@@ -8,11 +8,11 @@ var PodSchema = new Schema({
   image: {type: String, required: false},
   author: {type: String, required: false},
   copyright: {type: String, required: false},
-  episode_title: {type: String, required: false},
-  episode_description: {type: String, required: false},
+  episodeTitle: {type: String, required: false},
+  episodeDescription: {type: String, required: false},
   published: {type: String, required: true},
   url: {type: String, required: true, unique: true},
-  media_type: {type: String, required: false},
+  mediaType: {type: String, required: false},
   likes: {type: Number, required: false}
 })
 
