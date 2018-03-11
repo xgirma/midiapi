3c8aa2bcda4ce46c5a03170606cbefd4dce0e9fe
xgirma <xgirma@gmail.com>
2018-03-11 04:42:11 -0700

    set cors

--- middleware/middlware.js
+++ middleware/middlware.js
@@ -10,6 +10,7 @@ module.exports = function (app) {
   app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next()
   })
   app.use(cors({
